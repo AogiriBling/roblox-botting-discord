dc6ec489ccd8f99cbf8cbd03a6585aed1d05ef2e
Jason Bling <96725711+AogiriBling@users.noreply.github.com>
2022-07-28 19:22:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def add_user(cookie, userid):
 
 @bot.command()
 async def rfriend(ctx, userId):
-            embed = discord.Embed(color=0x0F0F0F, description=f"**__* <@{ctx.author.id}> Sent*__** `60` **__*Friend Requests*__**")
+            embed = discord.Embed(color=0x0F0F0F, description=f"**__*<@{ctx.author.id}> Sent*__** `60` **__*Friend Requests*__**")
             await ctx.send(embed=embed)
             for x in cookies1:
                 threading.Thread(target=add_user, args=(x, userId,)).start()
@@ -51,6 +51,26 @@ async def rfollow(ctx, userId):
             await ctx.send(embed=embed)
             for x in cookies1:
                 threading.Thread(target=follow_user, args=(x, userId,)).start()
+                
+           
+       
+    
+def visit_bot(cookie, gameid):
+    try:
+        with requests.session() as session:
+                session.cookies['.ROBLOSECURITY'] = cookie
+                session.headers['x-csrf-token'] = session.post('https://friends.roblox.com/v1/users/1/request-friendship').headers['x-csrf-token']
+                xsrf_token = session.post('https://auth.roblox.com/v1/authentication-ticket/', headers={'referer':f'https://www.roblox.com/games/{gameid}'}).headers['rbx-authentication-ticket']
+                browserId = random.randint(1000000, 10000000)
+                os.system(f'start roblox-player:1+launchmode:play+gameinfo:{xsrf_token}+launchtime:{browserId}+placelauncherurl:https%3A%2F%2Fassetgame.roblox.com%2Fgame%2FPlaceLauncher.ashx%3Frequest%3DRequestGame%26browserTrackerId%3D{browserId}%26placeId%3D{gameid}%26isPlayTogetherGame%3Dfalse+browsertrackerid:{browserId}+robloxLocale:en_us+gameLocale:en_us+channel:')
+    except:
+        pass
+
+
+@bot.command()
+async def rvisit(ctx, userId):
+            embed = discord.Embed(color=0x0F0F0F, description=f"**__*<@{ctx.author.id}> Sent*__** `100` **__*visits*__** `[BETA]`")
+            await ctx.send(embed=embed)
 
 
 bot.run(token)
